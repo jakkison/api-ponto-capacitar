d995521fb443126efd20305c30501808bd1c752c
Jacson Santos <jacsoncristiano1j6@gmail.com>
2021-07-29 08:44:14 -0300

    Adicionado suporte CI com Travis CI.

--- src/main/java/com/capacitar/pontoapi/PontoApiApplication.java
+++ src/main/java/com/capacitar/pontoapi/PontoApiApplication.java
@@ -1,9 +1,20 @@
 package com.capacitar.pontoapi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 @SpringBootApplication
+@EntityScan(basePackages = {"com.capacitar.pontoapi.model"})
+@ComponentScan(basePackages = {"com.*"})
+@EnableJpaRepositories(basePackages = {"com.capacitar.pontoapi.repository"})
+@EnableWebMvc
+@RestController
+@EnableAutoConfiguration
 public class PontoApiApplication {
 
 	public static void main(String[] args) {
