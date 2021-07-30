f911c5244cfa67b387d0256771314169970b5b7f
Jacson Santos <jacsoncristiano1j6@gmail.com>
2021-07-29 23:26:38 -0300

    Inserção de Entidades empresa/lancamento/funcionario

---
+++ src/main/java/com/capacitar/pontoapi/enums/PerfilEnum.java
@@ -0,0 +1,14 @@
+package com.capacitar.pontoapi.enums;
+
+
+
+public enum PerfilEnum {
+
+	// Role significa FUNÇÃO, Logo ROLE_ADMIN = FUNÇÃO_ADMIN
+	// Enum são dados estaticos sem necessidade de guardar no banco de dados.
+	
+	
+	ROLE_ADMIN,
+	ROLE_USUARIO;
+	
+}
---
+++ src/main/java/com/capacitar/pontoapi/enums/TipoEnum.java
@@ -0,0 +1,12 @@
+package com.capacitar.pontoapi.enums;
+
+public enum TipoEnum {
+
+	INICIO_TRABALHO,
+	TERMINO_TRABALHO,
+	INICIO_ALMOCO,
+	TERMINO_ALLMOCO,
+	INICIO_PAUSA,
+	TERMINO_PAUSA;
+	
+}
